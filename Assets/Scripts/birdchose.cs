bf5cdeac58ec67b5fd1acf21522bb2c0d7527030
kenbot10 <kbuddy19@icloud.com>
2025-11-07 10:39:02 -0500

    bird select script 1%

---
+++ Assets/Scripts/birdchose.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class birdchose : MonoBehaviour
+{
+    public Button terry;
+    public Button BlackBird;
+    public Button Whitebird;
+    public Button TicTacToebird;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
