21c10b32bb9d5c2bb754a6df84bdbe2c14a8bd35
TheLoggyBoi <147449860+TheLoggyBoi@users.noreply.github.com>
2025-11-11 10:11:49 -0500

    sws

---
+++ Assets/Scripts/birdchose.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class birdchose : MonoBehaviour
+{
+    public Dropdown.OptionDataList Dropdown;
+
+    public Transform terry1;
+    public Transform terry2;
+    public Transform Detonator1;
+    public Transform Detonator2;
+    public Transform TicTacToeBirds1;
+    public Transform TicTacToeBirds2;
+    public Transform darcy1;
+    public Transform darcy2;
+    public Transform BirdSlingshotLocatoin;
+}
