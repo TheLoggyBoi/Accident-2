d186a0f9e18937d23f7a2b31a2adf92f760dc8e2
TheLoggyBoi <147449860+TheLoggyBoi@users.noreply.github.com>
2025-11-11 10:36:27 -0500

    wswswsw

--- Assets/Scripts/BirdChoice.cs
+++ Assets/Scripts/BirdChoice.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Birdchose : MonoBehaviour
+public class BirdChoice : MonoBehaviour
 {
     [Header("References")]
     public Dropdown dropdown;
