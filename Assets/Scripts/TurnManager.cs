b55c210e480da473e41d8138dc63be2016d88cac
kenbot10 <kbuddy19@icloud.com>
2026-01-22 08:19:32 -0500

    just a fix

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -499,43 +499,35 @@ public class TurnManager : NetworkBehaviour
     {
         if (blackbird1.activeInHierarchy)
         {
-            player1Slingshot = blackbird1controller;
-            Debug.Log($"black bird check1");
+            player1Slingshot = blackbird1controller;    
         }
         if (blackbird2.activeInHierarchy)
         {
-            player2Slingshot = blackbird2controller;
-            Debug.Log($"black bird check2");
+            player2Slingshot = blackbird2controller;      
         }
         if (redbird1.activeInHierarchy)
         {
-            player1Slingshot = redbird1controller;
-            Debug.Log($"red bird check1");
+            player1Slingshot = redbird1controller;           
         }
         if (redbird2.activeInHierarchy)
         {
             player2Slingshot = redbird2controller;
-            Debug.Log($"red bird check2");
         }
         if (whitebird1.activeInHierarchy)
         {
-            player1Slingshot = whitebird1controller;
-            Debug.Log($"white bird check1");
+            player1Slingshot = whitebird1controller;            
         }
         if (whitebird2.activeInHierarchy)
         {
             player2Slingshot = whitebird2controller;
-            Debug.Log($"white bird check2");
         }
         if (tictacandtoe1.activeInHierarchy)
         {
-            player1Slingshot = tictacandtoe1controller;
-            Debug.Log($"tictacandtoe bird check1");
+            player1Slingshot = tictacandtoe1controller;     
         }
         if (tictacandtoe2.activeInHierarchy)
         {
             player2Slingshot = tictacandtoe2controller;
-            Debug.Log($"tictacandtoe bird check2");
         }
     }
 
