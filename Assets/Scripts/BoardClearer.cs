62226c8bbc8d3ac8180c3dd3fa9301815c40973b
LoginSoda <sug41010@gmail.com>
2026-02-25 08:41:39 -0500

    Bomb

---
+++ Assets/Scripts/BoardClearer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BoardClearer : MonoBehaviour
+{
+    // Drag and drop your X/O container objects or assign them in script
+    public GameObject[] symbols;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the character hits the board
+        if (collision.gameObject.CompareTag("Bomb"))
+        {
+            ClearBoard();
+        }
+    }
+
+    void ClearBoard()
+    {
+        // Deactivate all symbols on the board
+        foreach (GameObject symbol in symbols)
+        {
+            symbol.SetActive(false);
+        }
+        Debug.Log("Board Cleared!");
+        // Reset your game logic here
+    }
+}
