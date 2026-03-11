71b47b8d0d8660e92ed7f0066e8c8d6262919168
TheLoggyBoi <147449860+TheLoggyBoi@users.noreply.github.com>
2026-03-11 09:40:46 -0400

    bbp

--- Assets/Scripts/blueBirdPower.cs
+++ Assets/Scripts/blueBirdPower.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class blueBirdPower : MonoBehaviour
 {
-    // Reference to the Prefab you want to instantiate
-    public GameObject objectToDuplicate;
+    // Reference to the prefab you want to spawn.
+    // Make sure to assign this in the Unity Inspector!
+    public GameObject objectToSpawnPrefab;
 
-    void Update()
+    // Reference to a transform component, which dictates where the object spawns.
+    // Assign an empty GameObject in the scene to this in the Inspector.
+    public Transform spawnPoint;
+
+    // This public function will be called by the UI button.
+    public void SpawnObject()
     {
-        // Check if the 'Space' key is pressed down once
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Instantiate a clone of the prefab at the spawner's position and rotation
-            Instantiate(objectToDuplicate, transform.position, transform.rotation);
-        }
+        // Instantiate a clone of the prefab at the spawner's position and rotation.
+        Instantiate(objectToSpawnPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
