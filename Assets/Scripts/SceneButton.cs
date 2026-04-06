c31efac581abaac7c1148c8fb84df3b40fcf58ce
kenbot10 <kbuddy19@icloud.com>
2026-04-06 10:44:24 -0400

    how to play sence

--- Assets/Scripts/SceneButton.cs
+++ Assets/Scripts/SceneButton.cs
@@ -29,6 +29,11 @@ public class SceneButton : MonoBehaviour
 
     }
 
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("howtoplay");
+    }
+
     public void CreditsScene()
     {
         SceneManager.LoadScene("Credits");
